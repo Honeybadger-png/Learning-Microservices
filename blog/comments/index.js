6fdf894a75b1e70f41defaa97b3b131f547260f4
Honeybadger-png <9emertgul@gmail.com>
2024-04-07 20:55:39 +0300

    Moderation service function added for comments

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -18,7 +18,8 @@ app.post('/posts/:id/comments',async(req,res)=>{
     const commentId = randomBytes(4).toString('hex');
     const {content} = req.body;
     const comments = commentsByPostId[req.params.id] || [];
-    comments.push({id:commentId,content})
+    const defaultStatus = 'pending'
+    comments.push({id:commentId,content,status:defaultStatus})
 
     commentsByPostId[req.params.id] = comments;
 
@@ -27,15 +28,37 @@ app.post('/posts/:id/comments',async(req,res)=>{
         data:{
             id:commentId,
             content,
-            postId:req.params.id
+            postId:req.params.id,
+            status: defaultStatus
         }
     })
 
     res.status(201).send(comments)
 })
 
-app.post('/events',(req,res)=>{
-    console.log('Event Received',req.body.type)
+app.post('/events',async(req,res)=>{
+    const {type,data} = req.body
+    console.log('Event Received',type)
+    if(type==='CommentModerated'){
+        const {postId,id,status,content} = data;
+
+        const comments = commentsByPostId[postId];
+
+        const comment = comments.find(comment=>{
+            return comment.id === id
+        })
+        comment.status = status;
+
+        await axios.post('http://localhost:4005/events',{
+            type:'CommentUpdated',
+            data:{
+                id,
+                content,
+                postId,
+                status
+            }
+        })
+    }
 
     res.send({})
 })
--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -9,13 +9,16 @@ app.post('/events',(req,res)=>{
     const event = req.body;
 
     axios.post('http://localhost:4000/events',event).catch((err)=>{
-        console.log(err.message)
+        console.log("port 4000 : "+err.message)
     })
     axios.post('http://localhost:4001/events',event).catch((err)=>{
-        console.log(err.message)
+        console.log("post 4001 : "+err.message)
     })
     axios.post('http://localhost:4002/events',event).catch((err)=> {
-        console.log(err.message)
+        console.log("port 4002 : "+err.message)
+    })
+    axios.post('http://localhost:4003/events',event).catch((err)=>{
+        console.log("port 4003 : "+err.message)
     })
 
     res.send({status:'OK'});
