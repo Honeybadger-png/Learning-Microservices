444f58478d1ade74c5fa8f442cd857b80edc9281
Honeybadger-png <9emertgul@gmail.com>
2024-05-25 19:05:11 +0300

    i added new goodies

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -5,14 +5,18 @@ const axios = require('axios')
 const app = express()
 app.use(bodyParser.json())
 
+const events = [];
+
 app.post('/events',(req,res)=>{
     const event = req.body;
+    events.push(event);
+
 
     axios.post('http://localhost:4000/events',event).catch((err)=>{
         console.log("port 4000 : "+err.message)
     })
     axios.post('http://localhost:4001/events',event).catch((err)=>{
-        console.log("post 4001 : "+err.message)
+        console.log("port 4001 : "+err.message)
     })
     axios.post('http://localhost:4002/events',event).catch((err)=> {
         console.log("port 4002 : "+err.message)
@@ -22,6 +26,13 @@ app.post('/events',(req,res)=>{
     })
 
     res.send({status:'OK'});
+
+})
+
+app.get('/events',(req,res)=>{
+    
+    res.send(events)
+
 
 })
 
--- blog/posts/index.js
+++ blog/posts/index.js
@@ -40,5 +40,6 @@ app.post('/events',(req,res)=>{
 
 
 app.listen(4000,()=> {
+    console.log('v25');
     console.log("Listenin on 4000");
 })
