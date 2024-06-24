44f77601da04cc6943b57cee09f5d555246ca7d3
Honeybadger-png <9emertgul@gmail.com>
2024-06-24 20:57:21 +0300

    custom errors created and started to database management

--- ticketing/auth/src/errors/request-validation-error.ts
+++ ticketing/auth/src/errors/request-validation-error.ts
@@ -1,9 +1,10 @@
 import { ValidationError } from "express-validator";
+import { CustomError } from "./custom-error";
 
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError {
   statusCode = 400;
   constructor(public errors: ValidationError[]) {
-    super();
+    super('Invalid request parameters');
 
     // Only because we are extending a build in class
     Object.setPrototypeOf(this, RequestValidationError.prototype)
@@ -13,6 +14,8 @@ export class RequestValidationError extends Error {
     return this.errors.map((error) => {
       if(error.type === 'field'){
         return {message: error.msg, field: error.path};
+      }else{
+        return {message: error.msg};
       }
     })
   }
