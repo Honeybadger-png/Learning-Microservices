fff0221c75a18dfad8cc1d91bf0f1f6658b2cb81
Honeybadger-png <9emertgul@gmail.com>
2024-07-25 20:59:31 +0300

    i moved some common files to the common module that i created

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -2,9 +2,8 @@ import express,{Request,Response} from 'express'
 import { body } from 'express-validator';
 import { User } from '../models/user';
 
-import { BadRequestError } from '../errors/bad-request-error';
+import { BadRequestError,validateRequest } from '@m-gtickets/common';
 import jwt from 'jsonwebtoken';
-import { validateRequest } from '../middlewares/validate-request';
 
 
 const router = express.Router();
