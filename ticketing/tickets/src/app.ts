b913345ffb4676cb2fb7d443ffbc1da540796e41
Honeybadger-png <9emertgul@gmail.com>
2024-08-13 11:24:04 +0300

    axios vulnerability

--- ticketing/tickets/src/app.ts
+++ ticketing/tickets/src/app.ts
@@ -4,7 +4,8 @@ import { json } from 'body-parser'
 
 import cookieSession from 'cookie-session';
 
-import { errorHandler,NotFoundError } from '@m-gtickets/common'
+import { errorHandler,NotFoundError, currentUser } from '@m-gtickets/common'
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -13,6 +14,9 @@ app.use(cookieSession({
   signed: false,
   secure: process.env.NODE_ENV !== 'test'
 }))
+app.use(currentUser);
+
+app.use(createTicketRouter);
 
 
 app.all('*',async (req,res)=> {
